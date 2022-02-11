fdd4f7c1b79aa483fe50f7f0e09530712eed0df9
Jitendra Garg <jeetugarg69@gmail.com>
2022-02-12 00:37:16 +0530

    Goal Routes Protected and getting only User's Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,72 +1,75 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
 
-const Goal = require('../models/goalModel')
+const Goal = require("../models/goalModel");
 
 // @desc	Get goals
 // @route	GET /api/goals
 // @access	Private
 const getGoals = asyncHandler(async (req, res) => {
-	const goals = await Goal.find()
+	const goals = await Goal.find({ user: req.user.id });
 
 	// res.status(200).json({ message: 'Get Goals' })
-	res.status(200).json(goals)
-})
+	res.status(200).json(goals);
+});
 
 // @desc        Set goals
 // @route       POST /api/goals
 // @access      Private
 const setGoal = asyncHandler(async (req, res) => {
 	// console.log(req.body)
-	if(!req.body.text) {
+	if (!req.body.text) {
 		// res.status(400).json({ message: 'Please add a text feild!' })
-		res.status(400)
-		throw new Error('Please add a text feild!')
+		res.status(400);
+		throw new Error("Please add a text feild!");
 	}
 
 	const goal = await Goal.create({
-		text: req.body.text
-	})
+		text: req.body.text,
+		user: req.user.id,
+	});
 
 	// res.status(200).json({ message: 'Create Goals' })
-	res.status(200).json(goal)
-})
+	res.status(200).json(goal);
+});
 
 // @desc        Update goals
 // @route       PUT /api/goals/:id
 // @access      Private
 const updateGoal = asyncHandler(async (req, res) => {
-	const goal = await Goal.findById(req.params.id)
+	const goal = await Goal.findById(req.params.id);
 
-	if(!goal) {
-		res.status(400)
-		throw new Error('Goal Not Found!')
+	if (!goal) {
+		res.status(400);
+		throw new Error("Goal Not Found!");
 	}
 
-	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
+	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
 
 	// res.status(200).json({ message: `Goal number ${req.params.id} has been updated` })
-	res.status(200).json(updatedGoal)
-})
+	res.status(200).json(updatedGoal);
+});
 
 // @desc        Delete goals
 // @route       DELETE /api/goals/:id
 // @access      Private
 const deleteGoal = asyncHandler(async (req, res) => {
-	const goal = await Goal.findById(req.params.id)
+	const goal = await Goal.findById(req.params.id);
 
-        if(!goal) {
-                res.status(400)
-                throw new Error('Goal Not Found!')
-        }
+	if (!goal) {
+		res.status(400);
+		throw new Error("Goal Not Found!");
+	}
 
 	// const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
-	await goal.remove()
+	await goal.remove();
 
 	// res.status(200).json({ message: `Goal number ${req.params.id} has been deleted` })
-	
+
 	// This can be done but the remove method is more effective
 	// res.status(200).json(deletedGoal)
-	res.status(200).json({ id: req.params.id })
-})
+	res.status(200).json({ id: req.params.id });
+});
 
-module.exports = { getGoals, setGoal, updateGoal, deleteGoal }
+module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+const express = require("express");
+const router = express.Router();
+const {
+	getGoals,
+	setGoal,
+	updateGoal,
+	deleteGoal,
+} = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/').get(getGoals).post(setGoal)
+router.route("/").get(protect, getGoals).post(protect, setGoal);
 
 // router.get('/', getGoals)
 
 // router.post('/', setGoal)
 
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 // router.put('/:id', updateGoal)
 
 // router.delete('/:id', deleteGoal)
 
-module.exports = router
+module.exports = router;

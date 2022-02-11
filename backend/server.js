5ef92f7c44c6bb5979af5d47bd23ec26287a8119
Jitendra Garg <jeetugarg69@gmail.com>
2022-02-11 23:29:08 +0530

    Basic Bug fixes and user routes added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-const express = require("express")
-const colors = require('colors')
-const dotenv = require("dotenv").config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // This will overwrite the default express handler
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started at port ${port}`))
+app.listen(port, () => console.log(`Server started at port ${port}`));

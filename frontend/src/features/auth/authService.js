e364c24f1d421b1ee0191b1e81c1352d10bee7db
Jitendra Garg <jeetugarg69@gmail.com>
2022-02-17 16:06:35 +0530

    Added Logout Functionality

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,20 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = '/api/users/'
+const API_URL = "/api/users/";
 
 // Register User
 const register = async (userData) => {
-	const response = await axios.post(API_URL, userData)
+	const response = await axios.post(API_URL, userData);
 
-	if(response.data) {
-		localStorage.setItem('user', JSON.stringify(response.data))
+	if (response.data) {
+		localStorage.setItem("user", JSON.stringify(response.data));
 	}
 
-	return response.data
-}
+	return response.data;
+};
+
+// Logout User
+const logout = () => {
+	localStorage.removeItem("user");
+};
 
 const authService = {
-	register
-}
+	register, logout
+};
 
-export default authService
+export default authService;
